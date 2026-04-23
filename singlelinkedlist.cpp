c0edd505b1917afcac52e928de2d6bd0a253be68
tmglzx <tmglzx@gmail.com>
2026-04-23 20:32:11 +0700

    lengkapi fungsi addnode dengan duplikat check

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -61,4 +61,24 @@ return;
      previous->next = nodeBaru;
     }
 
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search (int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current ->noMhs)
+        {
+            previous = current;
+            current = current -> next;
+        }
+
+        return (current != NULL);
+    }
+
+
     
