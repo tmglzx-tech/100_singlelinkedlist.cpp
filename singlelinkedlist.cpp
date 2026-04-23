1e08081b02184b4e5af335016e306f7c36f17d39
tmglzx <tmglzx@gmail.com>
2026-04-23 20:34:53 +0700

    fungsi traverse untuk tampilkan data

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -96,4 +96,23 @@ return;
 
     }
 
-    
+    void traverse (){
+    if (listEmpty())
+    {
+        cout << "\nList Kosong\n";
+    }
+    else
+    {
+        cout << "\nData di dalam list adalah\n";
+        Node *currentNode = START;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << endl;
+            currentNode = currentNode->next;
+        }
+        cout << endl;
+    }
+}
+};
+
