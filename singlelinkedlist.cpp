e943f14c628e5bb70bc9ad9c7fc762aa570aa995
tmglzx <tmglzx@gmail.com>
2026-04-23 20:33:44 +0700

    dungsi delete node menggunakan search

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -81,4 +81,19 @@ return;
     }
 
 
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim,previous,current))
+        return false;
+
+        if (current == START)
+        START = START->next;
+        else previous->next = current->next;
+        delete current;
+        return true;
+
+    }
+
     
